a859ace7ee9ee24b963390cc48f9f1caa9fe00f9
Hamed Sedighi <hamedsedighi@gmail.com>
2021-04-19 00:58:45 +0200

    add keyloack utilities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './utility/app.guard';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, },
   {path: 'dashboard', component: DashboardComponent},
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+    canActivate: [AuthGuard]
   },
   // {path: '**', redirectTo: '/home'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
+import {initializeKeycloak} from './utility/app.init';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,17 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
